f9d10984b99a3d85c184ee22641ce0a3c6d1d9c1
Calin Salagean <salagean.calin@gmail.com>
2017-12-18 13:36:58 +0200

    Add draggable functionality

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#cap").draggable();
+    $("#corp").draggable();
+    $("#mana_stanga").draggable();
+    $("#picior_jos_dreapta").draggable();
+    $("#picior_jos_stanga").draggable();
+    $("#ureche_dreapta").draggable();
+    $("#ureche_stanga").draggable();
+});
