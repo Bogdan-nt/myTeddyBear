6b9949f0e68a52b957a3075042f727d3208b1528
bogdan-nt <bogdan.taraipan@gmail.com>
2018-01-08 23:30:34 +0200

    Staticaly-resized-images-and-changed-orientation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function () {
     $("#cap").draggable();
     $("#corp").draggable();
     $("#mana_stanga").draggable();
+    $("#mana_dreapta").draggable();
     $("#picior_jos_dreapta").draggable();
     $("#picior_jos_stanga").draggable();
     $("#ureche_dreapta").draggable();
