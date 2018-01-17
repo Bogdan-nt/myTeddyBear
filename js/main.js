cb2fddaa82f837e4f22ddf16bec4d6b5013b0643
alexandrustoica <13.01.1996ISbtQWERTY>
2018-01-17 12:46:14 +0200

    Place Components In The Right Place

--- js/main.js
+++ js/main.js
@@ -20,14 +20,32 @@ function allowDrop(event) {
 
 function drag(event) {
     event.dataTransfer.setData("id", event.target.id);
-
 }
 
 function drop(event) {
-    // TODO: Check Write Position Between Container and Component
     event.preventDefault();
-    const id = event.dataTransfer.getData("id");
-    event.target.appendChild(document.getElementById(id));
+    const component = document.getElementById(event.dataTransfer.getData("id"));
+    console.log(checkIfContainerIsValid(component, event.target));
+    console.log(checkIfContainerHasOtherChildren(event.target));
+    if (checkIfContainerHasOtherChildren(event.target) &&
+        checkIfContainerIsValid(component, event.target)) {
+        event.target.appendChild(component);
+    }
+}
+
+function checkIfContainerIsValid(component, container) {
+    return (container.id.indexOf(component.id) !== -1 &&
+        container.className === 'container_component') ||
+        container.className === 'button_container_form';
+}
+
+function checkIfContainerHasOtherChildren(container) {
+    return container.children.length === 0;
+}
+
+function checkIfComponentIsInTheRightContainer(component, container) {
+    console.log(container.id);
+    console.log(component.id);
 }
 
 function startBackgroundMusic() {
